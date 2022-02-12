b48c5c4910c4198fab0b0ee502963b5baba4fc4f
Lama Salah <lamasalah32@gmail.com>
2022-02-12 09:30:11 +0200

    Update insertion_sort.cpp

--- Codes/insertion_sort.cpp
+++ Codes/insertion_sort.cpp
@@ -1,29 +1,11 @@
 #include<bits/stdc++.h>
-#define ll long long
-#define ull unsigned long long
-#define all(vec) vec.begin(), vec.end()
-#define cin(v) for (auto& i : v) cin >> i
-#define cout(v) for (auto& i : v) cout << i << " "; cout << "\n"
-#define ceil(a, b) ((a / b) + (a % b ? 1 : 0))
-#define mod 1000000007
-
 using namespace std ;
-
-void code (){
-    ios_base :: sync_with_stdio (false), cin.tie(0), cout.tie(0);
-    #ifndef ONLINE_JUDGE
-    freopen("input.txt", "r", stdin);
-    freopen("output.txt","w",stdout);
-    #endif
-}
-
-
 
 int main(){
       code();
       int n; cin >> n;
       vector <int> arr(n);
-      for (auto& i : arr) cin >> i;  
+      for (auto& i : arr) cin >> i; 
 
       int k , j;
       for (int i = 1; i < n; i++){
@@ -40,4 +22,4 @@ int main(){
       cout << "\n";
     
     return 0;
-}      
+}
