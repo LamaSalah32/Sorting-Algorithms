14634721c2dc61bd68fbca17e615578dbe0de2cc
Lama Salah <lamasalah32@gmail.com>
2022-02-12 09:29:19 +0200

    Update QuickSort.cpp

--- Codes/QuickSort.cpp
+++ Codes/QuickSort.cpp
@@ -1,30 +1,20 @@
 #include<bits/stdc++.h>
-#define ll long long
-#define ull unsigned long long
-#define all(vec) vec.begin(), vec.end()
-#define cin(v) for (auto& i : v) cin >> i
-#define cout(v) for (auto& i : v) cout << i << " "; cout << "\n"
-#define ceil(a, b) ((a / b) + (a % b ? 1 : 0))
-#define mod 1000000007
-
 using namespace std ;
 
 int n;
 vector <int> arr;
 
 int part(int low, int high){
-
       int p = arr[high], k = low-1;
 
       for (int i = low; i <= high-1 ; i++){
             if (arr[i] < p){
-                  k++;       
+                  k++;   
                   swap(arr[i], arr[k]);
             }
       }
 
       swap (arr[k+1] , arr[high]);
-      cout(arr);
 
       return k+1;
 }
@@ -38,15 +28,15 @@ void QuickSort(int low, int high){
       }
 }
 
-
 int main(){
+      code();
       cin >> n;
       arr.resize(n);
-      for (auto& i : arr) cin >> i;  
+      for (auto& i : arr) cin >> i; 
       QuickSort(0, n-1);
       
-      for (auto& i : arr) cout << i << " "; 
+      for (auto& i : arr) cout << i << " ";
       cout << "\n";
     
     return 0;
-}      
+}   
